11e64db916bee7a8ec21003c5a09ed6f59559fc3
ignarodriguez1 <114880942+ignarodriguez1@users.noreply.github.com>
2023-02-27 17:36:25 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_7y940uq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Enviado!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+window.onscroll = function () {
+    
+    if (document.documentElement.scrollTop > 100) {
+
+        document.querySelector('.go-top-container ')
+            .classList.add('show');
+    } else {
+        document.querySelector('.go-top-container')
+            .classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
